a69388038a8724af4afe25fda7306937c47974e2
SantyMss <santiagomarse@hotmail.com>
2023-11-06 19:47:54 -0500

    Se agregan consultas por id

--- src/MenuArbol.java
+++ src/MenuArbol.java
@@ -88,7 +88,7 @@ public class MenuArbol {
                     nuevo.nom = nom;
                     nuevo.ciu = ciu;
                     nuevo.edad = edad;
-                    nuevo.semestre = semestre;
+                    nuevo.semestre = semestre;              
                     nuevo.sexo = sexo;
                     ab.insertar(nuevo);
                 }
@@ -133,6 +133,80 @@ public class MenuArbol {
         }
     }
 
+    public void Consultas() {
+        boolean salir = false;
+        do {
+            String opc = JOptionPane.showInputDialog(
+                    """
+                    MENU CONSULTAS
+                    1. Consulta por la (ID)
+                    
+
+
+
+
+
+                    0. Volver al menú principal
+                    Seleccione una opción: """);
+            switch (opc) {
+                case "1" -> consultaPorPK();
+                case "0" -> salir = true;
+                default ->
+                    JOptionPane.showMessageDialog(null, "Opción incorrecta");
+            }
+        } while (!salir);
+    }
+    
+    public void consultaPorPK() {
+
+
+        CargarDatos();
+
+
+        int id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID a consultar:"));
+        visitasRealizadas = 0; // Reiniciar el contador de visitas
+        long tiempoInicio = System.currentTimeMillis();
+        Nodo encontrado = buscarPorID(ab.raiz, id);
+        long tiempoFin = System.currentTimeMillis();
+            
+
+
+
+        if (encontrado != null) {
+            JOptionPane.showMessageDialog(null, "Nodo encontrado:\n" +
+                    "ID: " + encontrado.id + "\n" +
+                    "Apellido: " + encontrado.ape + "\n" +
+                    "Nombre: " + encontrado.nom + "\n" +
+                    "Visitas realizadas: " + visitasRealizadas);
+        } else {
+            JOptionPane.showMessageDialog(null, "Nodo con ID " + id + " no encontrado");
+        }
+        mostrarTiempoEjecucion(tiempoInicio, tiempoFin);
+}
+
+        private void mostrarTiempoEjecucion(long tiempoInicio, long tiempoFin) {
+            long tiempoTotal = tiempoFin - tiempoInicio;
+            JOptionPane.showMessageDialog(null, "Tiempo de ejecución: " + tiempoTotal + " milisegundos");
+        }
+            
+    
+            private int visitasRealizadas = 0;
+            
+            private Nodo buscarPorID(Nodo reco, int id) {
+                if (reco != null) {
+                    visitasRealizadas++;
+                    if (id == reco.id) {
+                        return reco; // Nodo encontrado
+                    } else if (id < reco.id) {
+                        return buscarPorID(reco.izq, id); // Buscar en el subárbol izquierdo
+                    } else {
+                        return buscarPorID(reco.der, id); // Buscar en el subárbol derecho
+                    }
+                }
+                return null; // Nodo no encontrado
+            }
+            
+
 
 
 
@@ -143,17 +217,17 @@ public class MenuArbol {
         do {
             String opc = JOptionPane.showInputDialog(
                     """
-                     
+                    MENU TALLER ARBOL BINARIO
                     1. Recorrido del árbol
-                    2. Pre-orden
-                    3. Entre-orden
-                    4. Pos-orden
+                    2. Menu Consultas
+                    3. 
+                    4. 
                     0. Salir
                     100. Crear datos aleatorios
                     Seleccione una opcion: """);
             switch (opc) {
                 case "1" -> RecorrerOrden();
-                case "2" -> ab.imprimirPre();
+                case "2" -> Consultas();
                 case "3" -> ab.imprimirEntre();
                 case "4" -> ab.imprimirPost();
                 case "0" -> salir = true;
@@ -177,8 +251,9 @@ public class MenuArbol {
         JOptionPane.showMessageDialog(null, 
                 """
                 CODIGO IMPLEMENTADO EN PC:
-                RAM: 16 GB
                 Procesador: AMD Ryzen™ 5 5600H - 3.3GHz - 6 nucleos / 12 hilos
+                Ram: 16GB DDR4 
+                Almacenamiento: 512GB SSD
                 Sistema operativo Windows 11
                 Arquitectura 64 bits
                 """);
